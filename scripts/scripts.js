ab83d71d3627b0bb62e709ee696ed605e1fe8f5c
ruby <rubycao04@gmail.com>
2022-11-22 10:16:21 -0800

    click once and removes

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -2,7 +2,19 @@ const clickMeButton = document.querySelector(".click-me");
 
 function handleclickMeButton () {
     alert("Hello! I regret all my life choices!!");
-
+    clickMeButton.removeEventListener("click", handleclickMeButton );
   }
 
   clickMeButton.addEventListener("click", handleclickMeButton );
+
+  
+
+
+
+
+
+
+
+
+
+
