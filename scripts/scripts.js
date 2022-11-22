0930518d7c1dd94f95352b36bbc90b1e4a4fee2e
ruby <rubycao04@gmail.com>
2022-11-22 10:23:01 -0800

    click button only once

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -2,11 +2,11 @@ const clickMeButton = document.querySelector(".click-me");
 
 function handleclickMeButton () {
     alert("Hello! I regret all my life choices!!");
-    clickMeButton.removeEventListener("click", handleclickMeButton );
+    // clickMeButton.removeEventListener("click", handleclickMeButton );
   }
 
-  clickMeButton.addEventListener("click", handleclickMeButton );
-
+  clickMeButton.addEventListener("click", handleclickMeButton, {once:true} );
+//   clickMeButton.removeEventListener("click", handleclickMeButton,);
   
 
 
