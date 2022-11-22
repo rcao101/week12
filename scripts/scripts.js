10d8e4d44225a86b768c034e2f709144a5871b21
ruby <rubycao04@gmail.com>
2022-11-22 10:09:41 -0800

    click me listener

---
+++ scripts/scripts.js
@@ -0,0 +1,8 @@
+const clickMeButton = document.querySelector(".click-me");
+
+function handleclickMeButton () {
+    alert("Hello! I regret all my life choices!!");
+
+  }
+
+  clickMeButton.addEventListener("click", handleclickMeButton );
